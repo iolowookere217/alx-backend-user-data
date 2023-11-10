a899446350396d7bf5123afe231a1fcbb24ec148
iolowookere217 <olowookere.isaaco@gmail.com>
2023-11-10 20:13:12 +0000

    Task 1

--- 0x01-Basic_authentication/api/v1/app.py
+++ 0x01-Basic_authentication/api/v1/app.py
@@ -13,6 +13,13 @@ app = Flask(__name__)
 app.register_blueprint(app_views)
 CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
 
+@app.errorhandler(401)
+def unauthorized_request(error) -> str:
+	""" Unauthorized_requests
+	"""
+
+    return jsonify({"error":"Unauthorized"}), 401
+
 
 @app.errorhandler(404)
 def not_found(error) -> str:
